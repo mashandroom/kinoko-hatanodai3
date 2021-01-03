a4022043a46299918827c6d1e10afcac17da2892
Makoto Murase <kinoko@kinoko-book.local>
2021-01-03 14:21:02 +0900

    add quiz json.

--- pages/index.vue
+++ pages/index.vue
@@ -8,10 +8,13 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { Vue, Component } from 'vue-property-decorator'
 import Question from '~/components/Question.vue'
 
-export default Vue.extend({})
+@Component
+export default class Index extends Vue {
+
+}
 </script>
 
 <style>
---
+++ pages/question.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    テスト
+  </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+import quizzes from '@/assets/json/quiz.json'
+@Component
+export default class Question extends Vue {
+  private mounted(): void {
+    console.log(this.$route.query.id)
+    console.log(quizzes[0])
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+</style>
