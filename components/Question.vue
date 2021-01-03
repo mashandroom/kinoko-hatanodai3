e17ddabbbdf2e4cb62564b026b8c85c57ed42067
h2m_kinoko <happy2mylife@gmail.com>
2021-01-03 11:01:37 +0900

    first commit.

---
+++ components/Question.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    キノコ-abcde-123-村瀬
+
+    <div class="map">
+      <img src="/img/map.png" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class Question extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.map img {
+  width: 100%;
+}
+</style>
